cfff060dfe41aed982fd788298f7c307aadd8a27
sf <sf@localhost.com>
2025-10-18 14:50:40 -0700

    Fixed error

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-// const { app, BrowserWindow, ipcMain } = require('electron');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const fs = require('fs');
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'renderer.js'),
-      contextIsolation: false,
-      nodeIntegration: true
+      contextIsolation: true,
+      nodeIntegration: false
     }
   });
 
@@ -19,8 +18,8 @@ function createWindow () {
 
 app.whenReady().then(createWindow);
 
-// Listen for the event to save JSON
 ipcMain.on('save-json', (event, jsonData) => {
   const savePath = path.join(app.getPath('documents'), 'StudyData.json');
   fs.writeFileSync(savePath, JSON.stringify(jsonData, null, 2));
+  console.log('Saved JSON to', savePath);
 });
